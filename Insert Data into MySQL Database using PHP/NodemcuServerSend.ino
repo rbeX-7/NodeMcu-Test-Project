6c1ce7a5cf3df79cddd973639c14473c99749b22
rbeX-7 <57653785+rbeX-7@users.noreply.github.com>
2019-11-14 23:47:02 +0700

    Create NodemcuServerSend.ino

---
+++ Insert Data into MySQL Database using PHP/NodemcuServerSend.ino
@@ -0,0 +1,89 @@
+/*
+  Credit to : Rui Santos
+  Original project at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/  
+*/
+
+#ifdef ESP32
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#else
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#endif
+
+//network credentials
+const char* ssid     = "<YOUR_SSID>";
+const char* password = "<YOUR_SSID_PASSWORD>";
+
+//omain name and URL path or IP address with path
+const char* serverName = "http://<YOUR_SERVER_IP_OR_DOMAIN_NAME>/post-esp-data.php";
+
+// Keep this API Key must be same with the PHP code provided in the project page. 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+void setup() {
+  Serial.begin(115200);  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    int a = random(1,10);
+    int b = random(1,10);
+    int c = random(1,10);
+    String httpRequestData = "api_key=" + apiKeyValue + "&a=" + a + "&b=" + b + "&c=" + c;
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+        
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 10 seconds
+  delay(10000);  
+}
